eb175753ca4fc1e9f0f337748fd469f4756180d4
techmmr <mmishra@grepruby.com>
2017-07-04 20:47:04 +0530

    interactive sales tax complete

--- app/generateTax.js
+++ app/generateTax.js
@@ -4,16 +4,18 @@ function generateTax(items) {
     let tax=0;
     let totalTax=0
     let total=0;
-    for(let i=0;i<items.length;i++){
-        if(items[i].imported==YES)
-	       taxPercentage+=5;
-        if(items[i].taxable==YES)
-	       taxPercentage+=10;
-    	tax=Math.round(items[i].cost*taxPercentage*20/100)/20;
-        items[i].tax=Number(tax.toFixed(3));
-        total+=items[i].cost+items[i].tax;
-        totalTax+=items[i].tax;
+    if(items.length>0){
+        for(let i=0;i<items.length;i++){
+            if(items[i].imported==YES)
+	          taxPercentage+=5;
+            if(items[i].taxable==YES)
+    	       taxPercentage+=10;
+            tax=Math.round(items[i].cost*taxPercentage*20/100)/20;
+            items[i].tax=Number(tax.toFixed(3));
+            total+=items[i].cost+items[i].tax;
+            totalTax+=items[i].tax;
+        }
+        console.log(`total cost : ${total} | total tax : ${totalTax}`);
     }
-    console.log(`total cost : ${total} | total tax : ${totalTax}`);
 }
 export {generateTax}
--- app/index.js
+++ app/index.js
@@ -1,4 +1,21 @@
-import http from 'http';
 import {generateTax} from './generateTax';
-import {createList} from './createList';
-generateTax(createList());
+const readline = require('readline');
+const TERMINATE = 'q'
+
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+
+let items = [];
+console.log('Enter Item Details As {ItemName ItemCost Imported(y/n) Taxable(y/n)} and \'q\' to finish entering : \n');
+rl.on('line', (answer) => {
+	if(answer==TERMINATE){
+	generateTax(items);
+	items = [];
+	}
+	else{
+		let result=answer.split(" ");
+		items.push({itemName:result[0], cost:Number(result[1]), imported:result[2], taxable:result[3]});
+	}
+});
