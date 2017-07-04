36e44cea8528809d43b10e31c4fb83bf41b00cd8
techmmr <mmishra@grepruby.com>
2017-07-04 19:53:34 +0530

    making sales tax interactive (in progress)

--- app/createList.js
+++ app/createList.js
@@ -1,7 +1,26 @@
+const prompt = require('prompt');
+const TERMINATE = 'q'
 function createList() {
-    let  items = [];
-    items.push({itemName:"name1", cost:40, imported:'y', taxable:'y'});
-    items.push({itemName:"name2", cost:50, imported:'n', taxable:'y'});
-    return items;
+//	const rl = readline.createInterface({
+//		input: process.stdin,
+//		output: process.stdout
+//	});
+	prompt.start();
+	let items = [];
+	console.log('Enter Item Details As {ItemName ItemCost Imported(y/n) Taxable(y/n)} and \'q\' to finish entering : \n');
+	// rl.on('line', (answer) => {
+	// 	if(answer==TERMINATE){
+	// 		console.log('terminated');
+	// 	}
+	// 	else{
+	// 		let result=answer.split(" ");
+	// 		items.push({itemName:result[0], cost:Number(result[1]), imported:result[2], taxable:result[3]});
+	// 	}
+	// 	rl.close();
+	// });
+	prompt.get(['itemName', 'ItemCost', 'imported', 'taxable'], function (err, result) {
+		items.push({itemName:result.itemName, cost:Number(result.ItemCost), imported:result.imported, taxable:result.taxable});
+  });
+	return items;
 }
 export {createList}
