f9f4a906b8a4dca25e74f6ffb8eccda5dcc65ba6
techmmr <mmishra@grepruby.com>
2017-07-04 18:38:25 +0530

    var changed to let(improves memory efficiency

--- app/createList.js
+++ app/createList.js
@@ -1,5 +1,5 @@
 function createList() {
-    var items = [];
+    let  items = [];
     items.push({itemName:"name1", cost:40, imported:'y', taxable:'y'});
     items.push({itemName:"name2", cost:50, imported:'n', taxable:'y'});
     return items;
--- app/generateTax.js
+++ app/generateTax.js
@@ -1,19 +1,18 @@
 const YES='y';
 function generateTax(items) {
-    var taxPercentage=0;
-    var tax=0;
-    var totalTax=0
-    var total=0;
-    console.log(items.length);
-    for(var i=0;i<items.length;i++){
+    let taxPercentage=0;
+    let tax=0;
+    let totalTax=0
+    let total=0;
+    for(let i=0;i<items.length;i++){
         if(items[i].imported==YES)
 	       taxPercentage+=5;
         if(items[i].taxable==YES)
 	       taxPercentage+=10;
     	tax=Math.round(items[i].cost*taxPercentage*20/100)/20;
-	items[i].tax=Number(tax.toFixed(3));
-	total+=items[i].cost+items[i].tax;
-	totalTax+=items[i].tax;
+        items[i].tax=Number(tax.toFixed(3));
+        total+=items[i].cost+items[i].tax;
+        totalTax+=items[i].tax;
     }
     console.log(`total cost : ${total} | total tax : ${totalTax}`);
 }
