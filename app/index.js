ccf2c5aa195795c9eb7e168240b1107eba03fdee
techmmr <mmishra@grepruby.com>
2017-07-05 12:54:41 +0530

    readline-sync, interactive CLI implemented

--- app/index.js
+++ app/index.js
@@ -1,21 +1,22 @@
 import {generateTax} from './generateTax';
-const readline = require('readline');
+const readlineSync = require('readline-sync');
 const TERMINATE = 'q'
 
-const rl = readline.createInterface({
-input: process.stdin,
-output: process.stdout
-});
-
 let items = [];
-console.log('Enter Item Details As {ItemName ItemCost Imported(y/n) Taxable(y/n)} and \'q\' to finish entering : \n');
-rl.on('line', (answer) => {
-	if(answer==TERMINATE){
-	generateTax(items);
-	items = [];
-	}
-	else{
-		let result=answer.split(" ");
-		items.push({itemName:result[0], cost:Number(result[1]), imported:result[2], taxable:result[3]});
-	}
+
+console.log('Commands : \n 1) Add Item Details As {add ItemName ItemCost Imported(y/n) Taxable(y/n)} \n 2) generateList as {generate} \n 3) Exit interface as {exit}.\n');
+readlineSync.promptCLLoop({
+add: 		function(name, cost, imported, taxable) {
+				items.push({itemName:name, cost:Number(cost), imported:imported, taxable:taxable});
+				console.log('added');
+			},
+generate: 	function() {
+				console.log('generating'); 
+				generateTax(items);
+				items=[];
+			},
+exit:		function() {
+				console.log('exitting...');
+				return true;
+			}
 });
